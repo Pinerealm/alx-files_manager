e36f22135359ccdd96f21785fdce5db4843a7dcd
Pinerealm <osalami09@gmail.com>
2023-12-03 17:15:22 +0300

    Add RedisClient class for Redis operations

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+// Define the class RedisClient
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.log(`Error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
