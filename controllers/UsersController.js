c021d711a13b1ff2b1d186c2efe17da42a839113
Pinerealm <osalami09@gmail.com>
2023-12-06 00:54:49 +0300

    Refactor the postNew static method

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -9,10 +11,24 @@ class UsersController {
 
     const emailExists = await dbClient.emailExists(email);
     if (emailExists) return res.status(400).send({ error: 'Already exist' });
-    const hashedPassword = sha1(password);
-    const user = await dbClient.createUser(email, hashedPassword);
+    const user = await dbClient.users.insertOne(
+      { email, password: sha1(password) },
+    );
 
-    return res.status(201).send({ id: user.id, email: user.email });
+    return res.status(201).send({ id: user.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.get('X-Token');
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    let userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+    userId = ObjectId(userId);
+
+    const user = await dbClient.users.findOne({ _id: userId });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    return res.status(200).send({ id: user._id, email: user.email });
   }
 }
 
